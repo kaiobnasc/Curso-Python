f2ae7bd7a484be5fa18bf9bbf566951802a99d57
Kaio Nascimento <kaionu2@gmail.com>
2023-10-02 00:02:35 -0300

    Estudando

---
+++ 008 - conversor de distancia.py
@@ -0,0 +1,5 @@
+m = float(input("Digite uma distância em metros: "))
+cm = m * 100
+mm = m * 1000
+
+print("A medida de {}m corresponde a {}cm e {}mm".format(m, cm, mm))
---
+++ 009 - tabuada.py
@@ -0,0 +1,12 @@
+numero = int(input("Digite o número que deseja ver a tabuada: "))
+
+print("{}x1 = {}".format(numero, numero*1))
+print("{}x2 = {}".format(numero, numero*2))
+print("{}x3 = {}".format(numero, numero*3))
+print("{}x4 = {}".format(numero, numero*4))
+print("{}x5 = {}".format(numero, numero*5))
+print("{}x6 = {}".format(numero, numero*6))
+print("{}x7 = {}".format(numero, numero*7))
+print("{}x8 = {}".format(numero, numero*8))
+print("{}x9 = {}".format(numero, numero*9))
+print("{}x10 = {}".format(numero, numero*10))
