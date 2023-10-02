e40d2f0145d012d579e2b8ee8748d663fed9ea8a
Kaio Nascimento <kaionu2@gmail.com>
2023-10-01 21:30:48 -0300

    Continuando estudos

---
+++ desafio6.py
@@ -0,0 +1,8 @@
+n1 = int(input("digite o número: "))
+dobro = n1*2
+triplo = n1*3
+raiz = n1**(1/2)
+
+print("O dobro de {} é {}".format(n1, dobro))
+print("O triplo de {} é {}".format(n1, triplo))
+print("A raiz de {} é {}".format(n1, raiz))
--- teste.py
+++
@@ -1,5 +0,0 @@
-n1 = int(input("Digite o primeiro número: "))
-n2 = int(input("Digite o segundo número: "))
-s = n1 + n2
-
-print("A soma de {} e {}, vale {}".format(n1, n2, s))
