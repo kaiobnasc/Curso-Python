ef857a1272ee23d365d53f52ea2ccee25b54d44a
Kaio <kaionu2@gmail.com>
2024-01-04 22:44:00 -0300

    Estudando

--- 053_DetectorDePalindromo.py
+++ 053_DetectorDePalindromo.py
@@ -1,3 +1,10 @@
 frase = str(input('Digite uma frase: ')).strip().upper()
-
-teste
+palavras = frase.split() #separa a frase em um vetor letra por letra
+junto = ''.join(palavras) #junta as strings usando o que tiver entre ''
+inverso = ''
+inverso = junto[::-1]
+print('O inverso de {} é {}'.format(junto, inverso))
+if inverso == junto:
+    print('Temos um palíndromo!')
+else:
+    print('A frase digitada não é um palindromo!')
